3c4edb1c5be305ff67ac34b94680316fd560bb35
Ashutosh Jadhav <ashutosh.jadhav@gmail.com>
2018-05-09 16:16:25 +0800

    Created a dummy domain model for employee to test if rest service is working fine

---
+++ src/main/java/com/neosis/services/Employee.java
@@ -0,0 +1,39 @@
+package com.neosis.services;
+
+public class Employee {
+    private long id;
+    private String firstName ;
+    private String lastName ;
+    private String employeeId;
+    public Employee (String employeeId){
+        this.employeeId = employeeId;
+        if(!employeeId.equalsIgnoreCase("0")) {
+            this.firstName = "Ashutosh";
+            this.lastName = "Jadhav";
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+}
