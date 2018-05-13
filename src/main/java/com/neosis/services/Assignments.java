941280a9a025361c43f95abf4218baf5213414eb
Ashutosh Jadhav <ashutosh.jadhav@gmail.com>
2018-05-13 15:23:06 +0800

    added new service class

---
+++ src/main/java/com/neosis/services/Assignments.java
@@ -0,0 +1,12 @@
+package com.neosis.services;
+
+public class Assignments {
+    private String[] assignments = {"assign1", "assign2"};
+    public Assignments(String empId) {
+
+    }
+
+    public String[] getAssignments() {
+        return assignments;
+    }
+}
